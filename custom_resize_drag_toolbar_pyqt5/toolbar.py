f38aa8b263b08fd0f94ea2e1428e873cdadce80e
saladeen <68706970+saladeen@users.noreply.github.com>
2021-10-01 15:10:20 -0400

    Update toolbar.py

--- custom_resize_drag_toolbar_pyqt5/toolbar.py
+++ custom_resize_drag_toolbar_pyqt5/toolbar.py
@@ -1,5 +1,4 @@
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel, QPushButton
-from PyQt5.QtGui import QIcon
 
 class CustomToolbar(QWidget):
     def __init__(self, parent, title_string: str):
